f7cf72040aa6acd83b34464afb23857687973f2f
Kristoffer Walker <kris@kixx.name>
2013-05-25 21:31:10 -0400

    installed Node.js with Chef
     On branch usr
    
    	modified:   cookbooks/nodejs/recipes/default.rb

--- cookbooks/nodejs/recipes/default.rb
+++ cookbooks/nodejs/recipes/default.rb
@@ -1,6 +1,16 @@
+package "build-essential"
+package "libssl-dev"
+
+remote_file "#{Chef::Config[:file_cache_path]}/node-v0.10.8.tar.gz" do
+  source "http://nodejs.org/dist/v0.10.8/node-v0.10.8.tar.gz"
+  action :create_if_missing
+end
+
 bash "install-node" do
+  cwd Chef::Config[:file_cache_path]
   code <<-EOH
-  echo "boooya"
+  tar -xzf node-v0.10.8.tar.gz
+  (cd node-v0.10.8 && ./configure && make && make install)
   EOH
   not_if "node --version 2>&1 | grep v0.10.8"
 end
