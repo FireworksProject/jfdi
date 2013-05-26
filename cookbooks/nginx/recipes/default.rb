328b9645333e86058233247f402099be5d1329f1
Kristoffer Walker <kris@kixx.name>
2013-05-25 22:49:26 -0400

    add config scripts to chef deployment
     On branch usr
    
    	modified:   chef/solo.json
    	new file:   cookbooks/nginx/recipes/default.rb
    	new file:   cookbooks/nginx/templates/default/sites-available-default.erb
    	new file:   cookbooks/php5-fpm/recipes/default.rb
    	new file:   cookbooks/php5-fpm/templates/default/php5-fpm-nginx.conf.erb
    	new file:   cookbooks/utils/recipes/default.rb

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,17 @@
+package "python-software-properties"
+
+bash "install-nginx-repository" do
+  code <<-EOH
+  add-apt-repository ppa:nginx/stable --yes
+  apt-get update
+  EOH
+end
+
+package "nginx"
+
+template "/etc/nginx/sites-available/default" do
+  source "sites-available-default.erb"
+  mode 0440
+  owner "root"
+  group "root"
+end
---
+++ cookbooks/utils/recipes/default.rb
@@ -0,0 +1,3 @@
+package 'curl'
+package 'vim'
+package 'tree'
