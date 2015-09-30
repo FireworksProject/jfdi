447f84d9efa5f343c48ee040dc59fb57fd7f779b
Kristoffer Walker <kris@kixx.name>
2015-09-29 23:33:07 -0400

    Provisioning script is almost working.
    Will need to use the open-jdk rather than Oracle to get Java going.
    	modified:   vm/provision/development.sh

--- vm/provision/development.sh
+++ vm/provision/development.sh
@@ -6,41 +6,55 @@ node_version="0.12.7"
 # Elastisearch versions https://www.elastic.co/downloads/elasticsearch
 elasticsearch_version="1.7.2"
 
-# export LANGUAGE=en_US.UTF-8
-# export LANG=en_US.UTF-8
-# export LC_ALL=en_US.UTF-8
-# locale-gen en_US.UTF-8
-# sudo dpkg-reconfigure locales
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
 
-# sudo apt-get update
-# # Prevent interactive menus during install
-# # http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
-# sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade --assume-yes
+sudo apt-get update
+# Prevent interactive menus during install
+# http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade --assume-yes
 
-# sudo apt-get autoremove --assume-yes
-# sudo apt-get autoclean --assume-yes
+sudo apt-get autoremove --assume-yes
+sudo apt-get autoclean --assume-yes
 
-# sudo apt-get install --assume-yes \
-#   build-essential \
-#   libssl-dev \
-#   curl \
-#   wget \
-#   vim \
-#   tree \
-#   htop
+sudo apt-get install --assume-yes \
+  build-essential \
+  libssl-dev \
+  curl \
+  wget \
+  vim \
+  tree \
+  htop
 
-# sudo apt-get autoremove --assume-yes
-# sudo apt-get autoclean --assume-yes
+sudo apt-get autoremove --assume-yes
+sudo apt-get autoclean --assume-yes
 
 echo "************************************************************"
 echo "Adding PPA repositories"
 echo "************************************************************"
-# sudo apt-get install python-software-properties
+sudo apt-get install python-software-properties
 
-# # Oracle Java is required for Elastisearch
-# sudo add-apt-repository ppa:webupd8team/java
-# sudo apt-get update
-# sudo apt-get install --assume-yes oracle-java8-installer
+# Oracle Java is required for Elastisearch
+sudo add-apt-repository --yes ppa:webupd8team/java
+
+# MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | \
+  sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+sudo apt-get update
+
+echo "************************************************************"
+echo "Installing Java"
+echo "************************************************************"
+debconf shared/accepted-oracle-license-v1-1 select true | \
+  sudo debconf-set-selections
+debconf shared/accepted-oracle-license-v1-1 seen true | \
+  sudo debconf-set-selections
+sudo apt-get install --assume-yes oracle-java8-installer
 
 echo "************************************************************"
 echo "Installing Node.js"
@@ -52,18 +66,19 @@ nvm install "$node_version"
 echo "************************************************************"
 echo "Installing Elastisearch"
 echo "************************************************************"
-# # Download and install the Public Signing Key
-# wget -q -O - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+wget "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$elasticsearch_version.deb"
+sudo dpkg -i "elasticsearch-$elasticsearch_version.deb"
+rm "elasticsearch-$elasticsearch_version.deb"
+# To test it all worked:
+# $ sudo /etc/init.d/elasticsearch start
+# $ curl http://localhost:9200
 
-# # Setup Repository
-# echo "deb http://packages.elastic.co/elasticsearch/${elasticsearch_version}/debian stable main" | \
-#   sudo tee -a /etc/apt/sources.list.d/elk.list
-
-# # Install Elasticsearch
-# sudo apt-get update && sudo apt-get install elasticsearch --assume-yes
-
-# # To test it all worked:
-# # curl http://localhost:9200
+echo "************************************************************"
+echo "Installing MongoDB"
+echo "************************************************************"
+sudo apt-get install --assume-yes mongodb-org
+# To test it all worked:
+# $ service mongod status
 
 echo "************************************************************"
 echo "Cleaning up"
