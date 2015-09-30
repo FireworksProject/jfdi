a14866244d1c94ffe5ff35caf32787cf544f63cf
Kristoffer Walker <kris@kixx.name>
2015-09-29 22:14:00 -0400

    Working Node.js install
    	modified:   vm/provision/development.sh

--- vm/provision/development.sh
+++ vm/provision/development.sh
@@ -1,36 +1,69 @@
 #!/usr/bin/env bash
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
-sudo dpkg-reconfigure locales
 
-sudo apt-get update
-# Prevent interactive menus during install
-# http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
-sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade --assume-yes
+# Node.js versions https://nodejs.org/dist/
+node_version="0.12.7"
 
-sudo apt-get autoremove --assume-yes
-sudo apt-get autoclean --assume-yes
+# Elastisearch versions https://www.elastic.co/downloads/elasticsearch
+elasticsearch_version="1.7.2"
 
-sudo apt-get install --assume-yes \
-  build-essential \
-  libssl-dev \
-  curl \
-  wget \
-  vim \
-  tree \
-  htop
+# export LANGUAGE=en_US.UTF-8
+# export LANG=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
+# locale-gen en_US.UTF-8
+# sudo dpkg-reconfigure locales
 
-sudo apt-get autoremove --assume-yes
-sudo apt-get autoclean --assume-yes
+# sudo apt-get update
+# # Prevent interactive menus during install
+# # http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+# sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade --assume-yes
+
+# sudo apt-get autoremove --assume-yes
+# sudo apt-get autoclean --assume-yes
+
+# sudo apt-get install --assume-yes \
+#   build-essential \
+#   libssl-dev \
+#   curl \
+#   wget \
+#   vim \
+#   tree \
+#   htop
+
+# sudo apt-get autoremove --assume-yes
+# sudo apt-get autoclean --assume-yes
+
+echo "************************************************************"
+echo "Adding PPA repositories"
+echo "************************************************************"
+# sudo apt-get install python-software-properties
+
+# # Oracle Java is required for Elastisearch
+# sudo add-apt-repository ppa:webupd8team/java
+# sudo apt-get update
+# sudo apt-get install --assume-yes oracle-java8-installer
 
 echo "************************************************************"
 echo "Installing Node.js"
 echo "************************************************************"
-curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
-source "$HOME/.bashrc"
-nvm install "0.12.7"
+wget -q -O - https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
+. /home/vagrant/.nvm/nvm.sh
+nvm install "$node_version"
+
+echo "************************************************************"
+echo "Installing Elastisearch"
+echo "************************************************************"
+# # Download and install the Public Signing Key
+# wget -q -O - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+# # Setup Repository
+# echo "deb http://packages.elastic.co/elasticsearch/${elasticsearch_version}/debian stable main" | \
+#   sudo tee -a /etc/apt/sources.list.d/elk.list
+
+# # Install Elasticsearch
+# sudo apt-get update && sudo apt-get install elasticsearch --assume-yes
+
+# # To test it all worked:
+# # curl http://localhost:9200
 
 echo "************************************************************"
 echo "Cleaning up"
